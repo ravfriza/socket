3b4a82a0ed0878a6d13b7cac193a8d148905d665
Daffa Ravfriza <ravfrizad@gmail.com>
2025-09-20 10:30:08 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+import express from 'express';
+import { createServer } from 'node:http'
+import { fileURLToPath } from 'node:url';
+import { join, dirname } from 'node:path';
+import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
+
+async function main() {
+    if (cluster.isPrimary) {
+        const numCPUs = availableParallelism();
+
+        for (let i = 0; i < numCPUs; i++) {
+            cluster.fork({
+                PORT: 3000 + i
+            })
+        }
+
+        setupPrimary();
+    } else {
+        const db = await open({
+            filename: 'chat.db',
+            driver: sqlite3.Database
+        })
+
+        await db.exec(`
+      CREATE TABLE IF NOT EXISTS messages (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          client_offset TEXT UNIQUE,
+          content TEXT
+      );
+    `);
+
+        const app = express();
+        const server = createServer(app);
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const io = new Server(server, {
+            connectionStateRecovery: {},
+            adapter: createAdapter()
+        });
+
+        app.get('/', (req, res) => {
+            res.sendFile(join(__dirname, 'index.html'));
+        })
+
+        io.on('connection', async (socket) => {
+            socket.on('chat message', async (msg, clientOffset, callback) => {
+                let result;
+                try {
+                    result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+                } catch (err) {
+                    if (err.errno === 19) {
+                        // Duplicate client_offset, already processed
+                        callback();
+                    } else {
+                        console.error('Database error:', err);
+                    }
+                    return;
+                }
+
+                io.emit('chat message', msg, result.lastID);
+
+                callback();
+            })
+
+
+            if (!socket.recovered) {
+                try {
+                    await db.each('SELECT id, content FROM messages where id > ?',
+                        [socket.handshake.auth.serverOffset || 0],
+                        (_err, row) => {
+                            socket.emit('chat message', row.content, row.id)
+                        }
+                    )
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+
+            socket.on('disconnect', () => {
+                console.log('a user disconnected')
+            })
+        })
+
+        const port = process.env.PORT;
+
+        server.listen(port, () => {
+            console.log('server running at http://localhost:' + port)
+        })
+    }
+}
+
+main()
